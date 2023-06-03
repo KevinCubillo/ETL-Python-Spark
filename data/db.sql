822b884988e489750e58cbae6936672479149d0e
KevinCubillo <kevincubillo25@gmail.com>
2023-06-02 23:54:34 -0600

    project

---
+++ data/db.sql
@@ -0,0 +1,24 @@
+CREATE TABLE etl (
+  id SERIAL PRIMARY KEY,
+  delito varchar(255),
+  subdelito varchar(255),
+  fecha VARCHAR(255),
+  hora VARCHAR(255),  
+  victima VARCHAR(255),
+  subvictima VARCHAR(255),
+  edad VARCHAR(255),
+  genero VARCHAR(255),
+  nacionalidad VARCHAR(255),
+  provincia VARCHAR(255),
+  canton VARCHAR(255),
+  distrito VARCHAR(255),
+  _c12 VARCHAR(255),
+  provincia1 VARCHAR(255),
+  canton1 VARCHAR(255),
+  distrito1 VARCHAR(255),
+  tasa_neta_de_participacion VARCHAR(255),
+  porcentaje_de_poblacion_economicamente_inactiva VARCHAR(255),
+  relacion_de_dependencia_economica VARCHAR(255)
+) 
+
+
